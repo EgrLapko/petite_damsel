2d1e9d8223bb743e0f78a77fa4160943f08394ee
Yehor Lapko <yehorlapko@zoho.com>
2019-05-06 12:19:29 +0800

    added some basic components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>I am footer!</h2>
+      </div>
+    )
+  }
+}
